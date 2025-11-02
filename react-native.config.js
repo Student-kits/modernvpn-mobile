d3f736924cd3771cd23c209b51f84dbc0e1f0378
Student-kits <learneducation616@gmail.com>
2025-11-02 10:11:04 +0530

    Initialize React Native project with VPN dependencies

---
+++ react-native.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dependencies: {
+    'react-native-vector-icons': {
+      platforms: {
+        ios: {
+          xcodeprojPath: 'ios/ModernVPN.xcodeproj',
+          plistPath: 'ios/ModernVPN/Info.plist',
+        },
+      },
+    },
+  },
+};
